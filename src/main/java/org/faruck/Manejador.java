4cab315d1f7bb1af34ae9172b10d24442fedee61
faruckchavarria <evafaruck@gmail.com>
2021-11-04 20:44:00 -0600

    Se crea la clase abstracta de Manejador, esperando una conexion de las demas clases.

---
+++ src/main/java/org/faruck/Manejador.java
@@ -0,0 +1,12 @@
+package org.faruck;
+
+public abstract class Manejador {
+    private Manejador sucesor;
+    public void setSucesor(Manejador sucesor) {
+        this.sucesor = sucesor;
+    }
+    public Manejador getSucesor() {
+        return sucesor;
+    }
+    public abstract double calculaCosteEnvio(Paquete p);
+}
