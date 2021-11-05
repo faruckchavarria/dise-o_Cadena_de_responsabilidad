7a15350dd1526a2dfb596e248e6e2c97135ae41d
faruckchavarria <evafaruck@gmail.com>
2021-11-04 21:38:24 -0600

    Se crea la clase de Paquete la cual permite compartir los gets, que usara los manejadores, de get peso, y get destino

---
+++ src/main/java/org/faruck/Paquete.java
@@ -0,0 +1,25 @@
+package org.faruck;
+
+public class Paquete {
+    private String contenido;
+    private String destino;
+    private double peso;
+
+    public Paquete(String contenido, String destino, double peso) {
+        this.contenido = contenido;
+        this.destino = destino;
+        this.peso = peso;
+    }
+
+    public String toString() {
+        return "### Paquete # Contenido: " + contenido + " # Destino: " + destino + " # Peso: " + peso;
+    }
+
+    public Object getDestino() {
+        return destino;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+}
